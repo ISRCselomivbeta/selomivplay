f03f5511ea6ad169942474cdb03d4dd1275272a7
ISRCselomivbeta <selomiv@gmail.com>
2026-02-06 00:31:25 -0300

    Update miv-backend.js

--- miv-backend.js
+++ miv-backend.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://script.google.com/macros/s/AKfycbxzAdN3bSQO-z_g2BhV7RPXEOvy5OJuFoJs6tXsyD0SYL5gOii8fomdffK1eQlq-Lo/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbxzAdN3bSQO-z_g2BhV7RPXEOvy5OJuFoJs6tXsyD0SYL5gOii8fomdffK1eQlq-Lo/exec ';
 
 function iniciarApp() {
   console.log('App iniciado com sucesso');
